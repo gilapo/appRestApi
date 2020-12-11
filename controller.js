6beff48162eee6e792b4f0894592e582b39e992e
gilapo <sulapohan.ss@gmail.com>
2020-12-11 17:04:55 +0800

    tampil data

--- controller.js
+++ controller.js
@@ -2,7 +2,31 @@
 
 var response = require('./res');
 var connection = require("./connect");
+const { query } = require('express');
 
 exports.index =function(req, res){
     response.ok("appRestApi is running", res)
+};
+
+exports.getAllData = function (req, res) {
+    connection.query("SELECT * FROM mahasiswa", function(error, rows, fields){
+        if (error) {
+            console.log(error);
+        }
+        else{
+            response.ok(rows, res)
+        }
+    });
+};
+
+exports.getAllDataId = function (req, res) {
+    let id = req.params.id;
+    connection.query("SELECT * FROM mahasiswa WHERE id = ?", [id],function(error, rows, fields){
+        if (error) {
+            console.log(error);
+        }
+        else{
+            response.ok(rows, res)
+        }
+    });
 };
--- routes.js
+++ routes.js
@@ -3,6 +3,12 @@
 module.exports = function (app) {
     var json = require('./controller')
 
-    app.route('/')
-        .get(json.index);
+    //home or root
+    app.route('/').get(json.index);
+
+    //get all data
+    app.route('/tampil').get(json.getAllData);
+
+    //get all data with id
+    app.route('/tampil/:id').get(json.getAllDataId);
 }
